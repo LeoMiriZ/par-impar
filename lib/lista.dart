be422415b3862d91bf285157120bd641dedff19b
Leonardo Miri <99846168+LeoMiriZ@users.noreply.github.com>
2024-06-04 18:21:41 -0300

    Update lista.dart

--- lib/lista.dart
+++ lib/lista.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'resultado.dart'; // Importe o arquivo resultado.dart
+import 'resultado.dart';
 
 class Lista extends StatefulWidget {
   final Function callback;
